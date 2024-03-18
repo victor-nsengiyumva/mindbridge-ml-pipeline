764776df6af68e3f09c8afa8a3f2a6898ef73655
IannnnnW <ianwamai@gmail.com>
2024-03-18 12:03:55 +0300

    Fixing Log errors

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -93,10 +93,10 @@ class DataTransformation:
         logging.info('Starting Transformation')
         
         for col in yes_no_cols:
-            features[col] = features[col].map({'YES': 1, 'NO': 0})
+            features[col] = features[col].map({'YES': 1, 'NO': 0}).astype(str)
 
         for column in frequency_columns:
-            features[column] = features[column].map({'Seldom': 0, 'Sometimes': 1, 'Usually': 2, 'Most-Often': 3})
+            features[column] = features[column].map({'Seldom': 0, 'Sometimes': 1, 'Usually': 2, 'Most-Often': 3}).astype(str)
 
         for column in from_columns:
             features[column] = features[column].astype(str).str.extract('(\d)')
