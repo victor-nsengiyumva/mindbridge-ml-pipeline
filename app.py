8476eaddcaf384c16a2687b6e5e1bf1d37f89859
IannnnnW <ianwamai@gmail.com>
2024-03-18 11:27:55 +0300

    Added port

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+import os
 import json
 import numpy as np
 import pandas as pd
@@ -45,4 +46,4 @@ def predict_datapoint():
     return jsonify({"diagnosis": classes[pred_pipeline.predict(pred_df)[0]]})
     
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)  
+    app.run(host="0.0.0.0", port=os.environ.get('PORT'))  
