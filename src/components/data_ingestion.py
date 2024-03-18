fc088341a3766cfd35a1e3cc85d0bceffde61819
IannnnnW <ianwamai@gmail.com>
2024-03-18 11:47:31 +0300

    Fixing Log errors

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -41,17 +41,17 @@ class DataIngestion:
         except Exception as e:
             raise CustomException(e, sys)
 
-if __name__ == '__main__':
-    obj = DataIngestion()
-    train_data, test_data = obj.initiate_data_ingestion()
+# if __name__ == '__main__':
+#     obj = DataIngestion()
+#     train_data, test_data = obj.initiate_data_ingestion()
 
-    data_transformation = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+#     data_transformation = DataTransformation()
+#     data_transformation.initiate_data_transformation(train_data, test_data)
 
-    train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
+#     train_arr,test_arr,_ = data_transformation.initiate_data_transformation(train_data, test_data)
 
-    modeltrainer = ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
+#     modeltrainer = ModelTrainer()
+#     print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
 
 
 
