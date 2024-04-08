d3136799a654170790c0c080efa3dfff134944be
IannnnnW <ianwamai@gmail.com>
2024-04-08 12:54:38 +0300

    Added ensemble model to pipeline from notebook

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -1,11 +1,14 @@
 import os
 import sys
+import numpy as np
 from dataclasses import dataclass
 from sklearn.model_selection import GridSearchCV
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import accuracy_score
 
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import r2_score
+from sklearn.svm import SVC
+
+from sklearn.ensemble import StackingClassifier
 
 from src.exception import CustomException
 from src.logger import logging
@@ -29,7 +32,7 @@ class ModelTrainer:
                 test_array[:,-1]
             )
 
-            #Random Forest Classifier
+            ##Random Forest Classifier
             param_grid = {
                 'n_estimators': [150, 250, 500],
                 'max_depth': [None, 30, 60],
@@ -48,25 +51,54 @@ class ModelTrainer:
 
             optimized_rf_classifier.fit(X_train, y_train)
 
-            y_pred_train = optimized_rf_classifier.predict(X_train)
             y_pred_test = optimized_rf_classifier.predict(X_test)
 
             train_accuracy = accuracy_score(y_test, y_pred_test)
 
             logging.info(train_accuracy)
 
-            logging.info('Done Choosing the best model')
-            
-            rf_accuracy = classification_report(y_test, y_pred_test)
+            ##SVM
+            svm_param_grid = {
+                'C': [0.1, 1, 10],                   
+                'kernel': ['linear', 'rbf', 'poly'],
+                'gamma': ['scale', 'auto'],          
+                'degree': [2, 3, 4],                 
+            }       
+
+            svm = SVC(random_state=42)
+            grid_search_svm = GridSearchCV(estimator = svm, param_grid = svm_param_grid)
+
+            grid_search_svm.fit(X_train, y_train)
+
+            best_params = grid_search_svm.best_params_
+            optimized_svm_classifier = SVC(**best_params, random_state = 42)
+
+            optimized_svm_classifier.fit(X_train, y_train)
+
+            y_pred_test_svm = optimized_svm_classifier.predict(X_test)
+
+            optimized_svm_accuracy = accuracy_score(y_test, y_pred_test_svm)
+            logging.info(optimized_svm_accuracy)
+
+            ##Ensemble Model
+            np.random.seed(42)
+            base_models = [
+                # ('logreg', LogisticRegression()),
+                ('svm', SVC()),
+                ('rf', RandomForestClassifier())
+            ]
+            stacking_clf = StackingClassifier(estimators=base_models, final_estimator=SVC())
+
+            stacking_clf.fit(X_train, y_train)
+
+            y_pred_test_stacking = stacking_clf.predict(X_test)
+
+            logging.info(accuracy_score(y_test, y_pred_test_stacking))
 
             save_object(
                 file_path=self.model_trainer_config.trained_model_file_path,
-                obj=optimized_rf_classifier
+                obj=stacking_clf
             )
-
-            r2_square = r2_score(y_test, y_pred_test)
-
-            return r2_square
                 
         except Exception as e:
             CustomException(e, sys)
